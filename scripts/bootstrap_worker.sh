5a30e25e27e89d468916b92651d3e997c4b25664
ziyad-tarek1 <ziyadtarek180@gmail.com>
2025-03-13 04:21:41 +0200

    Update bootstrap_worker.sh

--- scripts/bootstrap_worker.sh
+++ scripts/bootstrap_worker.sh
@@ -10,13 +10,13 @@ done
 # Read the token from the shared folder
 TOKEN=$(cat /vagrant/k3s-token)
 
-# Wait for the master API to be accessible (using netcat to check port 6443)
+# Wait for the master API to be accessible (port 6443)
 while ! nc -z ${K3S_MASTER_IP} 6443; do
   echo "Waiting for master API to be available..."
   sleep 2
 done
 
-# Install the K3s agent if not already installed
+# Install K3s agent if not already installed
 if ! command -v k3s >/dev/null; then
   curl -sfL https://get.k3s.io | \
     K3S_URL="https://${K3S_MASTER_IP}:6443" \
